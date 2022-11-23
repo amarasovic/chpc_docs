ae0470df5fd40a51f9e37dc5ee3b3d6f31b9298f
Ana Marasovic <ana.marasovic@utah.edu>
2022-11-23 08:36:03 -0700

    change ntasks

--- example.sh
+++ example.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account marasovic-gpu-np
 #SBATCH --partition marasovic-gpu-np
-#SBATCH --ntasks=32
+#SBATCH --ntasks-per-node=32
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:a100:1
 #SBATCH --time=8:00:00
